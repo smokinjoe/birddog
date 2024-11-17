66325cbcb69f6d8ed8843fe76afea0f07eaae43c
Joe <joe.ekiert@gmail.com>
2024-11-17 18:49:55 -0500

    implementing a buildResumeResponseObject method

--- src/handlers/resume.ts
+++ src/handlers/resume.ts
@@ -1,10 +1,31 @@
 import { assertIsDefined } from "../utils/assertions";
-import { getDB, getResumeId } from "../datastore/initializeDatabase";
+import { getDB } from "../datastore/initializeDatabase";
+import { InMemoryDatabase } from "../datastore/DatabaseWithTables";
+import { Resume } from "../types/Resume";
+
+function buildResumeResponseObject(db: InMemoryDatabase): Resume {
+  const baseResumeObject = db.getTable("Resume").first();
+  const address = db.getTable("Addresses").first();
+  const employmentHistory = db.getTable("EmploymentHistories").getAll();
+  const languages = db.getTable("Languages").getAll();
+  const projects = db.getTable("Projects").getAll();
+  const education = db.getTable("Schools").getAll();
+
+  const responseObject = {
+    ...baseResumeObject,
+    address,
+    employmentHistory,
+    languages,
+    projects,
+    education,
+  };
+
+  return responseObject;
+}
 
 export async function getResumeHandler() {
   const db = getDB();
-  const resumeId = await getResumeId();
-  const resume = db.getTable("resume").get(resumeId);
+  const resume = buildResumeResponseObject(db);
   assertIsDefined(resume, "Resume not found");
   return resume;
 }
